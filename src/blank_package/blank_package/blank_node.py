51253ba8726106655f77fd094414bd311a9feacf
Elias Holmen <eliasholmen@Student-Macbook-GH1XFH.local>
2026-01-28 11:45:23 +0200

    distance sensor commit attempt #1

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -3,24 +3,30 @@ import os
 import rclpy
 from rclpy.node import Node
 
-# Fill in something for msg type imports
-# from duckietown_msgs.msg import SOMETHING
-# from std_msgs.msg import SOMETHING
+from std_msgs.msg import Header
+from sensor_msgs.msg import Range
+from duckietown_msgs.msg import WheelsCmdStamped
 
-class SkeletonNode(Node):
+
+class DistanceNode(Node):
     def __init__(self):
-        super().__init__('blank_node')
-        #Create publishers and subscribers in init, use callback
-        pass
-    
-    #Define callback functions here
+        super().__init__('distance')
+        self.vehicle_name = os.getenv('VEHICLE_NAME')
+
+        self.distance_sub = self.create_subscription(Range, f'/{self.vehicle_name}/distance_sensor', self.check_range, 10)
+
+    def check_range(self, msg):
+        distance = msg.range
+        self.get_logger().info(f"Distance: {distance}")
+
+
+
 
 
 def main():
-    print('In main')
     rclpy.init()
-    node = SkeletonNode()
-    rclpy.spin(node)
+    distance_sensor = DistanceNode()
+    rclpy.spin(distance_sensor)
     rclpy.shutdown()
 
 
