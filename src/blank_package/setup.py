3f666e26a03659a182736b1b0211dc580fe4a1c5
adaiyrov <adaiyrov@isl.cy>
2026-01-29 14:53:05 +0200

    a

--- src/blank_package/setup.py
+++ src/blank_package/setup.py
@@ -10,12 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('lib/'+package_name,['blank_package/wheels.py']), #we added this line, add another if more nodes
-        ('lib/'+package_name,['blank_package/camera.py']),
-        ('lib/'+package_name,['blank_package/blinker.py']),
-        ('lib/'+package_name,['blank_package/compined_code.py']),
-        ('lib/'+package_name,['blank_package/publisher.py']),
-        ('lib/'+package_name,['blank_package/subscriber.py'])
+        ('lib/'+package_name,['blank_package/wheels.py'])
  ],
     install_requires=['setuptools'],
     zip_safe=True,
