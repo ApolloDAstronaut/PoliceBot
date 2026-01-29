b357042a2e526de95548d907aa802290c8d9c7d4
adaiyrov <adaiyrov@isl.cy>
2026-01-29 11:35:58 +0200

    a

--- src/blank_package/setup.py
+++ src/blank_package/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('lib/'+package_name,['blank_package/wheels.py']), #we added this line, add another if more nodes
         ('lib/'+package_name,['blank_package/camera.py']),
-        ('lib/'+package_name,['blank_package/camera.py'])
+        ('lib/'+package_name,['blank_package/blinker.py'])
  ],
     install_requires=['setuptools'],
     zip_safe=True,
